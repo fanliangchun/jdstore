675e6adfb35fd2679823ce05639891bbc024ebc9
fanliangchun <jeremydo2010@gmail.com>
2017-01-22 14:08:58 +0800

    implement admin/products

---
+++ app/controllers/admin/products_controller.rb
@@ -0,0 +1,51 @@
+class Admin::ProductsController < ApplicationController
+
+	before_action :find_products, only: [:show, :edit, :update, :destroy]
+
+	def show
+	end
+
+	def index
+		@products = Product.all
+	end
+
+	def new
+		@product = Product.new
+	end
+
+	def create
+		@product = Product.new(product_params)
+
+		if @product.save notice: "Product Created"
+			redirect_to admin_products_path
+		else
+			render :new
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @product.update(product_params)
+			redirect_to admin_products_path alert: "Product Updated"
+		else
+			render :edit
+		end
+	end
+
+	def destroy
+		@product.destroy
+		redirect_to admin_products_path alert: "Product Deleted"
+	end
+
+	private
+
+	def find_products
+		@product = Product.find(params[:id])
+	end
+
+	def product_params
+		params.require(:product).permit(:title, :description, :quantity, :price)
+	end
+end
